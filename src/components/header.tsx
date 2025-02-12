c1d48f679169850d21b68652c9a9c81b4fa8ddcf
Martin Klepsch <martinklepsch@googlemail.com>
2025-02-12 14:07:39 +0200

    header tweaks'

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,9 +19,9 @@ const labels = {
 }
 
 export function Header() {
-    const [location, setLocation] = useLocation();
+    const [location] = useLocation();
     return (
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="sticky top-0 flex h-12 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2 px-4">
                 <SidebarTrigger className="-ml-1" />
                 <Separator
@@ -35,8 +35,13 @@ export function Header() {
                                 {labels[location]}
                             </BreadcrumbLink>
                         </BreadcrumbItem>
-                        {/* <BreadcrumbSeparator className="hidden md:block" />
-                        <BreadcrumbItem>
+                        <BreadcrumbSeparator className="hidden md:block" />
+                        <BreadcrumbItem className="hidden md:block">
+                            <BreadcrumbLink href="#">
+                                All
+                            </BreadcrumbLink>
+                        </BreadcrumbItem>
+                        {/* <BreadcrumbItem>
                             <BreadcrumbPage>Data Fetching</BreadcrumbPage>
                         </BreadcrumbItem> */}
                     </BreadcrumbList>
