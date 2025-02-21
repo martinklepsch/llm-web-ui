0a89950843e178a7d4a4655bdd873494e3426379
Martin Klepsch <martinklepsch@googlemail.com>
2025-02-21 14:01:26 +0200

    add top level TooltipProvider

--- src/layouts/page.tsx
+++ src/layouts/page.tsx
@@ -10,18 +10,21 @@ import { ConversationsTable } from "@/pages/conversations-table";
 import { Link, Route, Switch } from "wouter";
 import ResponsesStream from "@/pages/responses-stream";
 import Usage from "@/pages/usage";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function Page() {
   const [location, setLocation] = useLocation();
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <Header />
-        <Route path="/" component={Usage} />
-        <Route path="/conversations" component={ConversationsTable} />
-        <Route path="/responses" component={ResponsesStream} />
-      </SidebarInset>
-    </SidebarProvider>
+    <TooltipProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <Header />
+          <Route path="/" component={Usage} />
+          <Route path="/conversations" component={ConversationsTable} />
+          <Route path="/responses" component={ResponsesStream} />
+        </SidebarInset>
+      </SidebarProvider>
+    </TooltipProvider>
   )
 }
